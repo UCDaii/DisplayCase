0632bf0666a12f1e8fad2afd30866b02d70c7833
grahamhency <grahamhency@gmail.com>
2015-06-10 22:06:54 -0400

    Category Population on Registration

--- registration.php
+++ registration.php
@@ -1,5 +1,6 @@
 <!--View-->
-
+<? include "_lib/scripts/mysqli_conn.php"; ?>
+<? include "_lib/scripts/categories.php"; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,7 +22,16 @@
 	<label>State: </label><input name="State" type="textbox" required="true"/> <br><!--Select with state abbreviation-->
 	<label>Country: </label><input name="Country" type="textbox" required="true"/> <br><!--Select with countries-->
 	<label>Major: </label><input name="MajorName" type="textbox" required="true"/> <br>
-	<label>Major Category: </label><input name="CategoryName" type="textbox" required="true"/><br> <!--Select with categories-->
+	<label>Major Category: </label>
+	<select name ="CategoryName">
+	<?
+	
+	foreach($CategoryName as $value):
+	echo '<option value="' .$value. '">' .$value. '</option>';
+	endforeach;
+	?>
+	</select>
+	<br> <!--Select with categories-->
 	<label>College: </label><input name="CollegeName" type="textbox" required="true"/><br>
 	<button type="submit">Submit</button>
 </form>
