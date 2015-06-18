a4d09df7e5f76988219d8997611626f01f6cbf64
usc012 <jmillerjr93@gmail.com>
2015-06-18 15:58:43 -0400

    updated sidebar links

--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,12 @@
 <div class="sidebar">
     <ul>
-        <li><a href="catagory.html" id="animationBackground"><img src="_lib/images/icons/light/an_lgt.svg"/><p>Animation</p></a></li>
-        <li><a href="catagory.html" id="fineartBackground"><img src="_lib/images/icons/light/art_lgt.svg"/><p>Fine Art</p></a></li>
-        <li><a href="catagory.html" id="culinaryBackground"><img src="_lib/images/icons/light/cul_lgt.svg"/><p>Culinary</p></a></li>
-        <li><a href="catagory.html" id="fashionBackground"><img src="_lib/images/icons/light/fash_lgt.svg"/><p>Fashion</p></a></li>
-        <li><a href="catagory.html" id="graphicBackground"><img src="_lib/images/icons/light/gd_lgt.svg"/><p>Graphic Design</p></a></li>
-        <li><a href="catagory.html" id="interiorBackground"><img src="_lib/images/icons/light/id_lgt.svg"/><p>Interior Design</p></a></li>
-        <li><a href="catagory.html" id="photographyBackground"><img src="_lib/images/icons/light/pht_lgt.svg"/><p>Photography</p></a></li>
-        <li><a href="catagory.html" id="websiteBackground"><img src="_lib/images/icons/light/web_lgt.svg"/><p>Web Design</p></a></li>
+        <li><a href="categories.php?CategoryId=1" id="animationBackground"><img src="_lib/images/icons/light/an_lgt.svg"/><p>Animation</p></a></li>
+        <li><a href="categories.php?CategoryId=2" id="fineartBackground"><img src="_lib/images/icons/light/art_lgt.svg"/><p>Fine Art</p></a></li>
+        <li><a href="categories.php?CategoryId=3" id="culinaryBackground"><img src="_lib/images/icons/light/cul_lgt.svg"/><p>Culinary</p></a></li>
+        <li><a href="categories.php?CategoryId=4" id="fashionBackground"><img src="_lib/images/icons/light/fash_lgt.svg"/><p>Fashion</p></a></li>
+        <li><a href="categories.php?CategoryId=5" id="graphicBackground"><img src="_lib/images/icons/light/gd_lgt.svg"/><p>Graphic Design</p></a></li>
+        <li><a href="categories.php?CategoryId=6" id="interiorBackground"><img src="_lib/images/icons/light/id_lgt.svg"/><p>Interior Design</p></a></li>
+        <li><a href="categories.php?CategoryId=7" id="photographyBackground"><img src="_lib/images/icons/light/pht_lgt.svg"/><p>Photography</p></a></li>
+        <li><a href="categories.php?CategoryId=8" id="websiteBackground"><img src="_lib/images/icons/light/web_lgt.svg"/><p>Web Design</p></a></li>
     <ul>
 </div>
